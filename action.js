322354e1fe08a2fc49a78a3cdc777897a6f8b6e1
engadnan-geo <adnangeo266@gmail.com>
2024-10-25 11:54:12 -0700

    i fineshed the project which was my job

--- action.js
+++ action.js
@@ -4,3 +4,33 @@ const navbar = document.querySelector('.navbar');
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+
+
+const btn1=document.querySelector(".btn")
+const btn2=document.querySelector(".btn1")
+const btn3=document.querySelector(".btn2")
+
+
+btn1.addEventListener("click",()=>{
+    window.location.href="cadinate/musharax1.html"
+}
+)
+
+
+btn2.addEventListener("click",()=>{
+    window.location.href="cadinate2/musharax2.html"
+}
+)
+
+btn3.addEventListener("click",()=>{
+    window.location.href="cadinate3/musharax3.html"
+}
+)
+
+
+
+
+const sigin=document.querySelector(".Sign")
+sigin.addEventListener("click",()=>{
+    window.location.href="login.html"
+})
