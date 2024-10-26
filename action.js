99cd5c11c46c2ccd39d471ffc747c375e840f0ab
engadnan-geo <adnangeo266@gmail.com>
2024-10-26 08:23:36 -0700

    waxaa kus soodaray qebta footerka iyo faq

--- action.js
+++ action.js
@@ -34,3 +34,48 @@ const sigin=document.querySelector(".Sign")
 sigin.addEventListener("click",()=>{
     window.location.href="login.html"
 })
+
+
+
+
+
+
+
+
+
+const faqs=[
+    {
+        question:"1.Eligibility to Vote?",
+        answer:"Only registered voters who meet registration requirements are allowed to participate in the election."
+    },
+    {
+        question:"2.One Vote Per Candidate?",
+        answer:"Each voter may cast only one vote for each position to ensure fairness and integrity in the election process."
+    },{
+        question:"3.Respectful Campaigning?",
+        answer:"All candidates and supporters must engage in respectful and fair campaigning, promoting a positive and inclusive environment for all voters."
+    },
+]
+
+
+
+const accordion=document.querySelector(".accordion")
+
+faqs.map((faq)=>{
+    const accordionItem=document.createElement("div")
+    accordionItem.classList.add("accordion-item")
+    accordionItem.innerHTML=`
+    <h3 class="question-box">${faq.question}</h3>
+    <p class="answer-box" style="display:none">${faq.answer}</p>    `;
+
+    const accordionContact=accordionItem.querySelector(".question-box")
+    accordionItem.querySelector(".answer-box").style.display="block"
+    accordionContact.addEventListener("click",()=>{
+        const accordionContacts=document.querySelectorAll(".answer-box")
+       accordionContacts.forEach(accordion=>{
+        accordion.style.display="none"
+       })
+     accordionItem.querySelector('.answer-box').style.display="block"
+    })
+    accordion.appendChild(accordionItem)
+})
