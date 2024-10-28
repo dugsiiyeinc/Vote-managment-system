c54dc8bcceb1b27388e5bce1750cbe9973cd0a8d
eng-mohamed-abdirahman <mohamedabdirahman1170@gmail.com>
2024-10-28 22:13:01 +0300

    doing some thing

--- js/musharax1.js
+++ js/musharax1.js
@@ -1,8 +1,15 @@
 const back = document.querySelector('.back');
+const voteNow = document.querySelector('.btn-vote')
 
 back.addEventListener('click',()=>{
     
      
-    window.location.href='./index.html'
+    window.location.href='index.html'
     
+})
+voteNow.addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log('helooo');
+    
+    window.location.href='form.html'  
 })
